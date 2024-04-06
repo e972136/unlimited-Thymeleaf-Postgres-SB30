aaa6fd0568fa2d40684b0e9815898ddeec52b6de
Gaspar <e972136@gmail.com>
2024-04-06 14:12:32 -0600

    Inicial

---
+++ src/main/java/com/unlimited/estimaciones/EstimacionesApplication.java
@@ -0,0 +1,13 @@
+package com.unlimited.estimaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unlimited/estimaciones/EstimacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlimited.estimaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
